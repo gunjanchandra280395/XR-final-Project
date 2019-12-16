133f2ad61a0b21647864f6560391126e16055b06
TheJuusto <lassi41@gmail.com>
2019-12-16 16:39:06 +0200

    Working two lens object hide and grabbable. Going to hit the gym :X

---
+++ Assets/MySTUFF/InvertTrack.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvertTrack : MonoBehaviour
+{
+    public bool stopRotation = false;
+    public bool stopTranslation = false;
+    public GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.camera = GameObject.Find("CenterEyeAnchor");
+    }
+
+    void checkButtons()
+    {
+        if (Input.GetKeyDown(KeyCode.P) || OVRInput.GetDown(OVRInput.Button.One))
+        {
+            stopRotation=!stopRotation;
+            this.initalRotEuler = this.camera.transform.rotation.eulerAngles;
+            initialRot = this.camera.transform.localRotation;
+            initialMockPos = this.transform.rotation;
+        }
+        if (Input.GetKeyDown(KeyCode.O) || OVRInput.GetDown(OVRInput.Button.Two))
+        {
+            stopTranslation=!stopTranslation;
+            initialPos = this.camera.transform.position;
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+           this.camera.transform.Rotate(Vector3.up,1);
+        }
+    }
+
+    public Quaternion initialMockPos;
+    public Vector3 initialPos;
+    public Quaternion initialRot;
+
+    public Vector3 initalRotEuler;
+    // Update is called once per frame
+    void Update()
+    {
+        checkButtons();
+//        if (stopRotation)
+//        {
+//            Quaternion diff = this.controller.transform.rotation *Quaternion.Inverse(initialRot);
+//            this.transform.rotation *= Quaternion.Inverse(diff);
+//            this.initialRot = this.controller.transform.rotation;
+//
+//        }
+       if (stopRotation)
+       {
+//          
+           this.transform.rotation = Quaternion.Inverse(this.camera.transform.localRotation * Quaternion.Inverse(initialRot));
+
+       }
+
+        if (stopTranslation)
+        {
+            this.transform.transform.position -= this.camera.transform.position - initialPos;
+
+        }
+
+
+    }
+}
---
+++ Assets/MySTUFF/StopTracking.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopTracking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.MCParent=GameObject.Find("Mock");
+        this.mainCamera = Camera.FindObjectOfType<OVRPlayerController>();
+    }
+
+    public bool verticalTrack = false;
+    public bool horizontalTrack = false;
+    public GameObject MCParent;
+    public OVRPlayerController mainCamera;
+    void checkButtons()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.One))
+            {
+            verticalTrack = !verticalTrack;
+
+        }
+        if (OVRInput.GetDown(OVRInput.Button.Two))
+        {
+            horizontalTrack = !horizontalTrack;
+
+        }
+
+    }
+
+    void resetCamera()
+    {
+        mainCamera. transform.localRotation =Quaternion.Euler(Vector3.zero);
+        mainCamera.transform.localPosition = Vector3.zero;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkButtons();
+
+        if (verticalTrack)
+        {
+            Vector3 mcPosition = mainCamera.transform.localPosition;
+
+            Quaternion camRot = mainCamera.transform.localRotation;
+            Vector3 eulerAngles = camRot.eulerAngles;
+            eulerAngles.x = 0;//MCParent.transform.rotation.eulerAngles.x;
+            eulerAngles.z = 0; //MCParent.transform.rotation.eulerAngles.z;
+            MCParent.transform.rotation = MCParent.transform.rotation  *Quaternion.Euler(eulerAngles);
+            
+            
+            
+            mcPosition.y = 0;
+            MCParent.transform.position += mcPosition; 
+            
+            
+            
+            
+        }
+        else if (horizontalTrack)
+        {
+            Vector3 mcPosition = mainCamera.transform.localPosition;
+            Quaternion camRot = mainCamera.transform.localRotation;
+            Vector3 eulerAngles = camRot.eulerAngles;
+            eulerAngles.y = 0;//MCParent.transform.rotation.eulerAngles.x;
+           
+            MCParent.transform.rotation = MCParent.transform.rotation  *Quaternion.Euler(eulerAngles);
+            
+            
+            
+           
+            mcPosition.x = 0;
+            mcPosition.z = 0;
+            MCParent.transform.position += mcPosition; 
+        }
+        else
+        {
+            
+            MCParent.transform.rotation = MCParent.transform.rotation* mainCamera.transform.localRotation;
+            MCParent.transform.position += mainCamera.transform.localPosition;
+           
+        
+        }
+        resetCamera();
+        
+        
+        
+        
+        
+    }
+}
