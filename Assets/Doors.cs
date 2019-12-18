4e26ed48a32a5d4be972868ab64a3696aa6e770e
secret624 <juho.m.rytkonen@gmail.com>
2019-12-18 17:27:25 +0200

    Door animation finished

---
+++ Assets/Doors.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    Animator animator;
+    bool doorOpen;
+    void Start()
+    {
+        doorOpen = false;
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Key")
+        {
+            doorOpen = true;
+            DoorsControl ("Open");
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if(doorOpen)
+        {
+            doorOpen = false;
+            DoorsControl ("Close");
+        }
+    }
+
+    void DoorsControl(string direction)
+    {
+        animator.SetTrigger(direction);
+    }
+}
